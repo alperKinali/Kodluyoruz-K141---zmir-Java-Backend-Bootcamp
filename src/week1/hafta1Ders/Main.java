c4d86f07e468d8dd20e3c690fa718dd63a4a2d07
Alper Kinali <alperkinali00@hotmail.com>
2022-08-02 01:19:08 +0300

    message added

--- src/week1/hafta1Ders/Main.java
+++ src/week1/hafta1Ders/Main.java
@@ -1,4 +1,7 @@
 package week1.hafta1Ders;
 
 public class Main {
+    public static void main(String[] args) {
+        System.out.println("Ders tekrarlarından akılda kalan kodlar buraya gelecek");
+    }
 }
